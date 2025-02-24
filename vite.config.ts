91ddc5b6d73fd115fd82b0f17a119cf66e634b66
liao1030 <liao1030@gmail.com>
2025-02-24 12:07:10 +0800

    Publish到Github網站

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Squares/', // 設置為您的 GitHub 倉庫名稱
 })
